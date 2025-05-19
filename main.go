22112f88f88da82708b4220248f3dc4b5f7cc322
Andrew Snodgrass <pointlander@gmail.com>
2025-05-19 13:20:35 -0500

    Use different formula

--- main.go
+++ main.go
@@ -16,10 +16,11 @@ func main() {
 	cube := func(a float64) float64 {
 		return a * a * a
 	}
+	_ = cube
 	c2dt2 := func(r float64) float64 {
 		const (
-			r_s    = 1.0
-			r_g    = r_s
+			r_s    = 33.0
+			r_g    = .01
 			c      = 299792458.0
 			dt     = 1
 			dr     = 1
@@ -28,12 +29,20 @@ func main() {
 			dphi   = 0
 		)
 		//return (1-r_s/r)*square(c)*dt + square(dr)/(1-r_s/r) + square(r)*(square(dtheta)+square(math.Sin(theta))*square(dphi))
-		return -square(3*math.Sqrt(1-r_s/r_g)-math.Sqrt(1-(square(r)*r_s)/cube(r_g)))*square(c)*square(dt)/4 +
-			square(dr)/(1-(square(r)*r_s)/cube(r_g)) +
+		//a := -square(3*math.Sqrt(1-r_s/r_g)-math.Sqrt(1-(square(r)*r_s)/cube(r_g))) * square(c) * square(dt) / 4
+		//b := square(dr) / (1 - (square(r)*r_s)/cube(r_g))
+		//d := square(r) * (square(dtheta) + square(math.Sin(theta))*square(dphi))
+		//fmt.Println(a, b, d)
+		//return a + b + d
+		//return -square(-math.Sqrt(1-square(r))) + 1/(1-square(r))
+		// https://physics.stackexchange.com/questions/530075/interpretation-of-the-interior-schwarzschild-solution
+		return (r_s/r_g)*(9-(6*r)/r_g)*square(dt) - square(dr) +
+			(r_s/r_g)*(square(r/r_g)*square(dr)-square(r_g/r)*square(dt)) -
 			square(r)*(square(dtheta)+square(math.Sin(theta))*square(dphi))
+
 	}
-	for i := 1.0; i >= 0.0; i -= .01 {
+	for i := 1.0; i > 0; i -= .1 {
 		fmt.Println(i, c2dt2(i), 1/math.Pow(i, 33))
 	}
-	fmt.Println(0, c2dt2(0), 1/math.Pow(0, 33))
+	//fmt.Println(0, c2dt2(0), 1/math.Pow(0, 33))
 }
